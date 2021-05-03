feb0c226b276bb6cb87f263831f586c83559d446
Charles Couillard <charles@benderlabs.io>
2021-05-03 18:00:48 +0200

    feat: simple monitoring endpoint

--- src/web/routes/Router.ts
+++ src/web/routes/Router.ts
@@ -1,9 +1,12 @@
-import {Router} from "express";
+import {Request, Response, Router} from "express";
 import LockRouter from "./LockRouter";
 import {StatisticsDependencies} from "../../indexers/StatisticsDependencies";
 
 function baseRouter(dependencies: StatisticsDependencies): Router {
   const router = new Router();
+  router.get("/monitoring", async (_req: Request, res: Response) => {
+    return res.json({"message": "ok"});
+  });
   router.use("/locks", LockRouter(dependencies));
   return router;
 }
