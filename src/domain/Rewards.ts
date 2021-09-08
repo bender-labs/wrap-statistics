a19a14b00b9a1411e97e9ef7bc4f422639c2a5a2
Charles Couillard <charles@benderlabs.io>
2021-09-08 17:27:47 +0200

    feat: implement rewards change on week #21

--- src/domain/Rewards.ts
+++ src/domain/Rewards.ts
@@ -22,13 +22,33 @@ const rewards: Record<number, number> = {
   1629727200000: 680993,
   1630332000000: 676208,
   1630936800000: 671456,
+  1631541600000: 666738,
+  1632146400000: 662054,
+  1632751200000: 657402,
+  1633356000000: 652782,
+  1633960800000: 648196,
+  1634565600000: 643641,
+  1635170400000: 639119,
+  1635775200000: 634628,
+  1636380000000: 630169,
+  1636984800000: 625741
 };
 
 const initialGlobalRewardsUserAllocation = 0.4;
 const globalRewardsUserAllocationFromWeekStarting1623679200000 = 0.12;
+const globalRewardsUserAllocationFromWeekStarting1631541600000 = 0.10;
+
+function getRewardRatePerStartDate(start: number) {
+  if (start >= 1631541600000) {
+    return globalRewardsUserAllocationFromWeekStarting1631541600000
+  } else if (start >= 1623679200000) {
+    return globalRewardsUserAllocationFromWeekStarting1623679200000
+  }
+  return initialGlobalRewardsUserAllocation;
+}
 
 export function getTokenRewardForPeriod(start: number, end: number, token: Token): number {
-  const globalRewardsUserAllocation = (start >= 1623679200000) ? globalRewardsUserAllocationFromWeekStarting1623679200000 : initialGlobalRewardsUserAllocation;
+  const globalRewardsUserAllocation = getRewardRatePerStartDate(start);
   let globalRewardForPeriod = 0;
   Object.keys(rewards).forEach(key => {
     const rewardStart = parseInt(key);
